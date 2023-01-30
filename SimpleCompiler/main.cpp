16d9969710c122fdb5809f46e0a5c6bffb609746
Kai Blaschke <kai.blaschke@kb-dev.net>
2023-01-30 11:54:06 +0100

    Parse input as string buffer in reentrant scanner.

--- SimpleCompiler/main.cpp
+++ SimpleCompiler/main.cpp
@@ -4,11 +4,38 @@ extern "C" {
 #include "Scanner.h"
 }
 
+#include <iostream>
+#include <sstream>
+
+static int ScanString(const char* input)
+{
+    int result{};
+    prjm_eel_compiler_context_t cctx;
+    yyscan_t scanner{};
+
+    prjm_eel_lex_init(&scanner);
+    YY_BUFFER_STATE bufferState = prjm_eel__scan_string(input, scanner);
+
+    result = prjm_eel_parse(&cctx, scanner);
+
+    prjm_eel__delete_buffer(bufferState, scanner);
+    prjm_eel_lex_destroy(scanner);
+
+    return result;
+}
+
 int main (int argc, char const* argv[])
 {
-    prjm_eel_compiler_context_t cctx;
-    yyscan_t scanner;
-    prjm_eel_lex_init(&scanner);
+    int result{};
+    {
+        {
+            std::stringstream programStream;
+            programStream << std::cin.rdbuf();
+            result = ScanString(programStream.str().c_str());
+        }
 
-    return prjm_eel_parse(&cctx, &scanner);
+        std::cout << "Parsing done." << std::endl;
+    }
+
+    return result;
 }
